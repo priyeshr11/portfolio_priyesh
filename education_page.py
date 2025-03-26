daf1d9be32abf728011dddc36f66c93e7b4a724a
Priyesh Dhole <61550823+priyeshr11@users.noreply.github.com>
2025-03-26 15:45:59 +0000

    Update education_page.py

--- education_page.py
+++ education_page.py
@@ -23,3 +23,18 @@ st.write("""
     Relevant Courses: Algorithms, Data Structures, Database Management Systems, Programming Languages
     """)
 
+
+st.write("Explore More About Me:")
+
+col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+    st.button("Education", on_click=show_page, args=("education_page",))
+with col2:
+    st.button("Work Experience", on_click=show_page, args=("work_experience",))
+with col3:
+    st.button("Sports Interest", on_click=show_page, args=("sports_page",))
+with col4:
+    st.button("Academic Experience", on_click=show_page, args=("academics",))
+with col5:
+    st.button("Research Projects", on_click=show_page, args=("research_work",))
